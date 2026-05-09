3606cc4787b4052ad251e6d0d75a08b17c2086d2
A200315862 <Pranav.Hingane@t-systems.com>
2026-05-09 18:16:48 +0530

    Initial ddl setup

---
+++ ddl/creat_iceberg_table.sql
@@ -0,0 +1,25 @@
+CREATE TABLE IF NOT EXIST `stellar-operand-384014.atomic_orders.orders (
+    row_id INTEGER PRIMARY KEY,
+    order_id VARCHAR(50) NOT NULL,
+    order_date DATE,
+    ship_date DATE,
+    ship_mode VARCHAR(50),
+    customer_id VARCHAR(50),
+    customer_name VARCHAR(255),
+    segment VARCHAR(50),
+    country VARCHAR(100),
+    city VARCHAR(100),
+    state VARCHAR(100),
+    postal_code VARCHAR(20),
+    region VARCHAR(50),
+    product_id VARCHAR(50),
+    category VARCHAR(100),
+    sub_category VARCHAR(100),
+    product_name VARCHAR(255),
+    sales DECIMAL(10, 2)
+)
+PARTITION BY order_date
+OPTIONS(
+  table_format = 'ICEBERG'
+);
+
