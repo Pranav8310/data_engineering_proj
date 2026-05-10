04812582e1a3bc28ae6c3b4cc7920f4a15e4725a
A200315862 <Pranav.Hingane@t-systems.com>
2026-05-10 18:23:05 +0530

    Fix: version

--- ddl/creat_iceberg_table.sql
+++ ddl/creat_iceberg_table.sql
@@ -2,5 +2,5 @@ CREATE OR REPLACE EXTERNAL TABLE `stellar-operand-384014.atomic_orders.orders`
 WITH CONNECTION `us.my-biglake-connection`
 OPTIONS (
   format = 'ICEBERG',
-  uris = ['gs://write_gcs/iceberg-warehouse/atomic_orders/orders/metadata/v4.metadata.json']
+  uris = ['gs://write_gcs/iceberg-warehouse/atomic_orders/orders/metadata/v5.metadata.json']
 );
